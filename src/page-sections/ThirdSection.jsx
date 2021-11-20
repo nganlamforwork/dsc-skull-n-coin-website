527cfba1863474cf02e24edaf72d5bd08336be66
nganlam0403 <nganlamforwork@gmail.com>
2021-11-20 23:43:37 +0700

    update project overview and fixing carousel

--- src/page-sections/ThirdSection.jsx
+++ src/page-sections/ThirdSection.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import SimpleTab from './components/SimpleTab.jsx';
-
+import { makeStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
+import thumbnail from '.././img/thumbnail-vertical.png';
 
 const useStyles = makeStyles(theme =>({
     root:{
@@ -10,37 +11,97 @@ const useStyles = makeStyles(theme =>({
         overflowX: "hidden",
         overflowY: "hidden",
         textAlign: "center",
-        paddingTop: "8%",
+        paddingTop: "10%",
     },
-    mainHeader:{
-        fontSize: 60,
-        letterSpacing: "0.2px",
-        marginBottom: 14
+    overview:{
+        display: "flex",
+        margin: "0 5%",
     },
-    subHeader:{
-        fontSize: 20,
-        lineHeight: "30px",
-        fontWeight: "lighter",
-        margin: "0 30%",
-        marginBottom: "6%"
+    image:{
+        maxWidth: "40%",
+        "& img":{
+            width:"70%"
+        }
     },
-    gallery:{
-        margin: "auto 20%",
-        color: "black"
+    content:{
+        marginRight:"5%",
+        maxWidth: "60%",
+        textAlign:"left",
+        "& span":{
+            fontSize: "1.7rem",
+            color:"#757de8"
+        },
+        "& h1":{
+            fontSize:60,
+            margin:"3% 0"
+        },
+        "& h2":{
+            margin:"3% 0 7% 0"
+        },
+        "& p":{
+            fontSize:"1.15rem",
+            lineHeight: "1.7rem"
+        }
+    },
+    subtitle:{
+        display: "flex",
+        alignItems: "center",
+        fontSize: 14,
+        "& h2":{
+            margin: "0 0 0 18px"
+        }
     }
 }));
 
 export default function ThirdSection() {
     const classes = useStyles();
-    const theme = useTheme();
 
     return (
     <div className={classes.root} id="project">
-        <div className={classes.Header}>
-            <h1 className={classes.mainHeader} >PROJECT</h1>
-            <h4 className={classes.subHeader}>Skull n Coin Game</h4>
+        <div className={classes.overview}>
+            <div className={classes.image}>
+                <img src={thumbnail} alt="thumbnail"/>
+            </div>
+            <div className={classes.content}>
+                <span>GAME PROJECT</span>
+                <h1>SKULL N COIN</h1>
+                <h2>SUPER RELAXING GAME</h2>
+                <Grid container spacing={3}>
+                    {infos.map(info => (
+                        <Grid item className={classes.gridItem} key={info.id} xs={12} md={6} >
+                            <div className={classes.subtitle}>
+                                <DoneOutlineIcon style={{color:"#757de8"}}/> 
+                                <h2>{info.title}</h2>
+                            </div>
+                            <p>{info.description}</p>
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>
         </div>
     </div>
     )
 }
 
+const infos = [
+    {
+        id:1,
+        title: "Thể loại",
+        description: "Game lật bài, trí nhớ, đua thời gian"
+    },
+    {
+        id:2,
+        title: "Công cụ phát triển",
+        description: "Game Maker Studio"
+    },
+    {
+        id:3,
+        title: "Thời gian phát hành",
+        description: "Ver1 - 11/2021"
+    },
+    {
+        id:4,
+        title: "Yêu cầu",
+        description: "Trực tuyến, sử dụng trình duyệt Opera"
+    },
+]
